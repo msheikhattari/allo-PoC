ee304693a2f76bb67116d35ef651db0a48ea1a24
0xdeo <0xdeo@defi.sucks>
2024-04-04 02:26:56 -0400

    chore: init from https://github.com/immunefi-team/forge-poc-templates at 85fa958

---
+++ test/PoCTest.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@immunefi/src/PoC.sol";
+import "../src/SandwichAttackContract.sol";
+
+contract PoCTest is PoC {
+    SandwichAttackContract public attackContract;
+    IERC20[] tokens;
+
+    function setUp() public {
+        // Fork from specified block chain at block
+        vm.createSelectFork("https://rpc.ankr.com/eth"); // , block_number);
+
+        // Deploy attack contract
+        attackContract = new SandwichAttackContract();
+
+        // Fund attacker contract
+        // e.g. deal(EthereumTokens.USDC, address(attackContract), 1 * 10 ** 10);
+
+        // Tokens to track during snapshotting
+        // e.g. tokens.push(EthereumTokens.USDC);
+
+        setAlias(address(attackContract), "Attacker");
+
+        console.log("\n>>> Initial conditions");
+    }
+
+    function testAttack() public snapshot(address(attackContract), tokens) {
+        attackContract.frontRun();
+
+        // Victim transaction
+
+        attackContract.backRun();
+    }
+}
